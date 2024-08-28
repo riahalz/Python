4885227e1dad24f474c886a3a0e873efd5a67ae0
Riya <161592266+riahalz@users.noreply.github.com>
2024-08-28 12:07:08 +0530

    Create While Loop with Break Statement.py

---
+++ Break and Continue Statements/While Loop with Break Statement.py
@@ -0,0 +1,13 @@
+count = 0
+
+while count<10:
+    if count==5:
+        break # terminates the loop
+    print("inside the loop")
+    count=count+1
+else:
+    print("inside else block")
+    
+
+    
+    
