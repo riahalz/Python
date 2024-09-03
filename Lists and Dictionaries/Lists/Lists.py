fb35da5b5c3ddb81f9ac84e2688920eb6c960c0c
Riya <161592266+riahalz@users.noreply.github.com>
2024-09-03 12:17:11 +0530

    Create Lists.py

---
+++ Lists and Dictionaries/Lists/Lists.py
@@ -0,0 +1,14 @@
+### List ###
+# Values in a list can be of different types - integer, string, tuple, boolean, etc.
+
+List = [10,2,3,'A','C4','3.3']
+# Index = 0,1,2,3,4,5
+
+
+# print values in the list by specifying their indexes
+print (List[0:4]) # print values with index 0-3
+print (List[5]) # print value with index 5
+
+
+# negative indexing - index is from right to left side of list, starting from 1
+print (List[-3]) # print value with negative in
