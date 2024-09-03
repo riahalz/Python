bb2b44f76a2533680bb3f85f3571955f389b6d1d
Riya <161592266+riahalz@users.noreply.github.com>
2024-09-03 10:21:09 +0530

    Add files via upload

---
+++ Lists and Dictionaries/Lists/Lists - Print List of Numbers in a Range.py
@@ -0,0 +1,6 @@
+# For Loop - Print numbers in a range
+
+# starting value in range = 1 (or any value you want)
+# ending value in range = 500 (or any value you want)
+# step/increase by = 2 (or any value you want)
+print(list(range(1,500,2)))
