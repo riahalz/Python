60a43ff1054089457045876997e856e81f06a622
Riya <161592266+riahalz@users.noreply.github.com>
2024-09-04 17:48:57 +0530

    Create For Loop.py

---
+++ For Loop/For Loop.py
@@ -0,0 +1,17 @@
+### for loop ###
+
+# starting value = 1 (or any other value)
+# ending value = 6 (or any other value)
+# step/increase by = any value/undefined (if undefined, step value = 1)
+
+for i in range (1,6):
+    print (i)
+
+# With step value
+for i in range (1,30,2):
+    print (i)
+
+# Print list of values in the same vertical line
+for i in range(105, 6, -7):
+    print (i, end = " ")
+    
