57c2b77615a2cf0b998745ac924a8b6923f92da0
Riya <161592266+riahalz@users.noreply.github.com>
2024-09-07 18:21:41 +0530

    Add files via upload

---
+++ Tuples/Tuples - Index of Nested Tuple:List in a Tuple.py
@@ -0,0 +1,15 @@
+### Index of value of Nested Tuples/Lists inside a Tuple ###
+
+
+# This is a tuple having a list and another tuple inside it.
+tuple5 = (1, 2, "a", (3, 4), [10, 20])
+
+
+# Index of value inside list in the tuple
+tuple5[4][0]
+print(tuple5[4][0])
+
+
+# Index of value inside tuple in the tuple
+tuple5[3][1]
+print(tuple5[3][1])
