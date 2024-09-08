663869c6c38aeea30a30faaa4e5cf4d0210c79c3
Riya Halbhavi <161592266+riahalz@users.noreply.github.com>
2024-09-08 07:52:19 +0530

    Create Check if Leap Year.py

---
+++ If, Else, Elif/Examples/Check if Leap Year.py
@@ -0,0 +1,14 @@
+### Check if a year is a leap year ###
+
+year = int(input("Enter a year: "))
+
+if (year%100)==0:
+    if (year%400)==0:
+        print(year, " is a leap year.")
+    else:
+        print(year, " is not a leap year.")
+else:
+    if (year%4)==0:
+        print (year, " is a leap year.")
+    else:
+        print (year, " is not a leap year.")      
