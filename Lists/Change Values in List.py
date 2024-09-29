6bb66bfba1c922f8d1b8e1b5a5e560451f7abece
Riya Halbhavi <halbhaviriya@gmail.com>
2024-09-29 12:32:28 +0530

    Delete Lists directory

--- Lists/Change Values in List.py
+++
@@ -1,9 +0,0 @@
-### Lists - Changing Values in a List ###
-
-list = [1,2,3,4,5,6,7,8]
-
-# Assigning a different value to an index
-list[5]="AAA"
-
-# Printing the updated list
-print (list)
--- Lists/reverse() - Reverse Order of Elements in List.py
+++
@@ -1,8 +0,0 @@
-### Reverse the order of elements in a list ###
-
-
-List1 = ["a","b","c","d","e"]
-
-# Reverse the order of elements in the list
-
-print (List1.reverse())
