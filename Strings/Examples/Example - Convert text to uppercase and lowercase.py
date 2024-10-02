921ea43433f146cf160f3e3b37d793fab1dfea2d
Riya Halbhavi <halbhaviriya@gmail.com>
2024-10-02 09:01:23 +0530

    Add files via upload

---
+++ Strings/Examples/Example - Convert text to uppercase and lowercase.py
@@ -0,0 +1,13 @@
+### 2. Convert value of text to upper case and lower case ###
+
+## Convert lowercase text to uppercase
+text = "text"
+# upper()
+text.upper()
+print (text.upper()) # Output: TEXT
+
+## Convert uppercase text to lowercase
+text = "TEXT"
+# lower()
+text.lower()
+print (text.lower()) # Output: text
