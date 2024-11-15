93a021bef6db0c6f0a2f0bc2cf9a1f3871421c87
Riya Halbhavi <halbhaviriya@gmail.com>
2024-11-15 07:16:31 +0530

    Update in() - Check if Values are Present in List.py

--- Lists/in() - Check if Values are Present in List.py
+++ Lists/in() - Check if Values are Present in List.py
@@ -1,7 +1,8 @@
-### Lists - Checking if Values are Present in a List ###
+### in() - Check if Values are Present in List ###
 
 a = [10, 20, 30, 40, 50]
 b = [60, 70, 80, 90, 100]
 
-print (40 in a) # Output: True
-print (20 in b) # Output - False
+# check if value is present in list: value in list
+print(40 in a) # Output: True
+print (20 in b) # Output: False
