da1443d00e4916757e7ec39a241133bcee4a3669
Riya Halbhavi <halbhaviriya@gmail.com>
2024-11-15 07:14:10 +0530

    Update copy() - Make Copy of List.py

--- Lists/copy() - Make Copy of List.py
+++ Lists/copy() - Make Copy of List.py
@@ -1,8 +1,8 @@
-### Lists - Make a Copy of a List ###
-
+### Make a Copy of a List ###
 
 List1 = ["carrot", "potato", "lettuce", "beans", "corn", "capsicum"]
 
 # print multiple copies of the same list
-print (List1.copy())
-print (List1.copy())
+List1.copy()
+print(List1.copy())
+print(List1.copy())
