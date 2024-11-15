118089a62f5aad25d4cf48f013960db19bca26b0
Riya Halbhavi <halbhaviriya@gmail.com>
2024-11-15 07:12:35 +0530

    Update extend() - Extend:Add Elements to End of List.py

--- Lists/extend() - Extend:Add Elements to End of List.py
+++ Lists/extend() - Extend:Add Elements to End of List.py
@@ -4,8 +4,8 @@ List1 = ["carrot", "potato", "lettuce", "beans", "corn"]
 List2 = ["a","b","c","d","e"]
 
 # Add elements from List2 to end of List1
-
 List1.extend(List2)
 
+# print updated list
 print(List1)
 # Output: ['carrot', 'potato', 'lettuce', 'beans', 'corn', 'a', 'b', 'c', 'd', 'e']
