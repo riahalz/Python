dba8ec7671e8bb5f4ff1d594206f02a7f7e0daad
Riya Halbhavi <halbhaviriya@gmail.com>
2024-11-15 07:17:19 +0530

    Update clear() - Clear Values in List.py

--- Lists/clear() - Clear Values in List.py
+++ Lists/clear() - Clear Values in List.py
@@ -1,10 +1,9 @@
-### Lists - Clear Values ###
-
+### clear() - Clear Values in List ###
 
 List1 = ["carrot", "potato", "lettuce", "beans", "corn", "capsicum"]
 
 # Clear all the values in the list
 List1.clear()
 
-# print the updated list to verify
-print (List1)
+# print the updated list
+print(List1)
