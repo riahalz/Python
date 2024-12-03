9afce2d4a976ec16844dd332958e259311e3a59c
Riya Halbhavi <halbhaviriya@gmail.com>
2024-12-03 13:45:02 +0530

    Update Print nth Fibonacci Number.py

--- Functions/Recursive Functions/Examples/Print nth Fibonacci Number.py
+++ Functions/Recursive Functions/Examples/Print nth Fibonacci Number.py
@@ -1,15 +1,15 @@
 ### Return nth Fibonacci number ###
 
 def fibonacci(n):
-    if n <= 0:
-        return True
-    elif n == 1:
+    if n < 0:
+        return "Input must be a positive integer"
+    elif n == 0:
         return 0
-    elif n == 2:
+    elif n == 1:
         return 1
     else:
         return fibonacci(n-1) + fibonacci(n-2)
 
-n = int(input("Enter the max number: "))
+n = int(input("Enter the position of the Fibonacci number to find (starting from 0): "))
 result = fibonacci(n)
-print (result)
+print(f"The {n}-th Fibonacci number is: {result}")
